8390d8b357d020c87b94a70b8babb0b7308ccebd
HennadiiBorysevych <genuch3@gmail.com>
2023-01-16 23:27:24 +0200

    home-tasks

--- js/task-09.js
+++ js/task-09.js
@@ -2,12 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6,0)}`;
 }
 
+
+
 const colorChangeBtn = document.querySelector(".change-color")
 let currentColor = document.querySelector(".color")
 const bodyBackground = document.body;
 
 colorChangeBtn.addEventListener("click", () => {
-  
-  bodyBackground.style.backgroundColor = getRandomHexColor();
-  currentColor.textContent = bodyBackground.style.backgroundColor;
+  const color = getRandomHexColor(); 
+  bodyBackground.style.backgroundColor = color;
+  currentColor.textContent = color;
 })
